9b95ebe1e3985d273c545d47568514b757cf90a6
Maiu <25614237+Maiu15@users.noreply.github.com>
2020-05-18 08:58:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 include('lib/sellix.php');
-$sellix = new Sellix('Od1q3S50OVFEXekSSWOwXEcaIfNsLKuKkCVGyvqH4yIceLaXDeO0uieB7FJw6q3O');
+$sellix = new Sellix('api_key_here');
 
-echo $sellix->getProduct('5ec1ae42e2a0b');
+echo $sellix->getProduct('product_id');
 ?>
